234a16a8d67498ec1be6cb279ef1fa1eaffb1a2e
caiomarqs <caiomarques1412@gmail.com>
2024-02-12 21:39:35 -0300

    undefined_return

--- src/front.js
+++ src/front.js
@@ -21,23 +21,70 @@ function printTimeZone() {
 
 let firstClick = 0;
 
-const playMusic = () => {
- 
+// const playMusic = () => {
+//         const sound = new Audio("src/audios/miss_you.mp3")
+//         sound.loop = true;
+//         sound.volume = 0.2
+//         sound.play();
+//         document.getElementById('music-name').innerHTML = "miss_you.mp3";
+//         firstClick = firstClick + 1;
+//         return
+// }
+
+// document.body.addEventListener("mousedown", (event) => {
+//     if(firstClick < 1) {
+//         playMusic();   
+//     }
+// })
+
+// document.body.addEventListener("touchstart", (event) => {
+//     if(firstClick < 1) {
+//         playMusic();   
+//     }
+// })
+
+function autoplayUnlock(element) {
+    var context = new (window.AudioContext || window.webkitAudioContext)();
+
+    return new Promise(function (resolve, reject) {
+        if (context.state === 'suspended') {
+            var unlock = function unlock() {
+                context.resume()
+                    .then(function () {
+                        window.removeEventListener('keydown', unlock);
+                        element.removeEventListener('click', unlock);
+                        element.removeEventListener('touchstart', unlock);
+                        element.removeEventListener('touchend', unlock);
+
+                        resolve();
+                    }).catch(function (error) {
+                        reject(error);
+                    });
+            };
+
+            window.addEventListener('keydown', unlock, false);
+            element.addEventListener('click', unlock, false);
+            element.addEventListener('touchstart', unlock, false);
+            element.addEventListener('touchend', unlock, false);
+        } else {
+            resolve();
+        }
+    });
+}
+
+var autoplayUnlockElement = document;
+
+autoplayUnlock(autoplayUnlockElement)
+    .then(function () {
         const sound = new Audio("src/audios/miss_you.mp3")
         sound.loop = true;
         sound.volume = 0.2
         sound.play();
         document.getElementById('music-name').innerHTML = "miss_you.mp3";
-        firstClick = firstClick + 1;
-        return
-
-}
-
-document.body.addEventListener("mousedown", (event) => {
-    if(firstClick < 1) {
-        playMusic();   
-    }
-})
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
 
 startTime()
 printTimeZone()
